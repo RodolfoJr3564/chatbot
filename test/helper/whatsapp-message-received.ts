f5c535085a7a85fdb43d8aaf28e776bd454f3fca
Rodolfo <rodolfojr94@gmail.com>
2023-08-21 17:30:23 -0300

    feat: Add webhook integration tests

---
+++ test/helper/whatsapp-message-received.ts
@@ -0,0 +1,31 @@
+import { ReceivedWhatsappMessageType } from "../../types"
+
+export const whatsappMessageReceived: (
+  data?: Partial<ReceivedWhatsappMessageType>,
+) => ReceivedWhatsappMessageType = (data = {}) => ({
+  messaging_product: "whatsapp",
+  metadata: {
+    display_phone_number: "15550272790",
+    phone_number_id: "112831885206437",
+  },
+  contacts: [
+    {
+      profile: {
+        name: "Rodolfo",
+      },
+      wa_id: "554796834984",
+    },
+  ],
+  messages: [
+    {
+      from: "554796834984",
+      id: "wamid.HBgMNTU0Nzk2ODM0OTg0FQIAEhgWM0VCMDlCQUM5NkVDRjI0RUJBNkFDMAA=",
+      timestamp: "1690810992",
+      text: {
+        body: "text",
+      },
+      type: "text",
+    },
+  ],
+  ...data,
+})
